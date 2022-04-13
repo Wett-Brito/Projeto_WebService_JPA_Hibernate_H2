174347a5bf547e3b380b7f0b107c896e3b829cf4
Wellington Brito <wellington1000@yahoo.com.br>
2022-04-13 00:55:17 -0300

    Primeiro commit, primeira Entity e Resource criadas

---
+++ src/main/java/com/wellington/curso/UserResource.java
@@ -0,0 +1,20 @@
+package com.wellington.curso;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wellington.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@teste.com", "11954911322", "123456789");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
