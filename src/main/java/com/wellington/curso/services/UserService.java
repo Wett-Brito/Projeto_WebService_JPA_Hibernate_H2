8d4cedf13542c495bb0747b31fbee6beddc4e40c
Wellington Brito <wellington1000@yahoo.com.br>
2022-04-23 12:51:26 -0300

    Tratamento de exceção - findById

--- src/main/java/com/wellington/curso/services/UserService.java
+++ src/main/java/com/wellington/curso/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.wellington.curso.entities.User;
 import com.wellington.curso.repositories.UserRepository;
+import com.wellington.curso.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
 	public User findById(Long id) {
 		
 		Optional<User> obj =  reporistory.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
