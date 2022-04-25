dc6250f3b7040c595642ff30404e6832084eb3c8
Wellington Brito <wellington1000@yahoo.com.br>
2022-04-25 01:11:49 -0300

    Adicionando uma nova exception dessa vez para o metodo DELETE,

--- src/main/java/com/wellington/curso/services/UserService.java
+++ src/main/java/com/wellington/curso/services/UserService.java
@@ -4,44 +4,58 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.wellington.curso.entities.User;
 import com.wellington.curso.repositories.UserRepository;
+import com.wellington.curso.services.exceptions.DatabaseException;
 import com.wellington.curso.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
-	private UserRepository reporistory;
+	private UserRepository repository;
 	
 	public List<User> findAll(){
-		return reporistory.findAll();
+		return repository.findAll();
 	}
 	
 	public User findById(Long id) {
 		
-		Optional<User> obj =  reporistory.findById(id);
+		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
-		return reporistory.save(obj);
+		return repository.save(obj);
 	}
 	
 	public void delete(User obj) {
-		reporistory.delete(obj);
+		try {
+			repository.delete(obj);
+			
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(obj.getId());
+		}
 	}
 	
 	public void deleteById(Long id) {
-		reporistory.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User UpdateById(Long id, User obj) {
-		User entity = reporistory.getById(id);
+		User entity = repository.getById(id);
 		updateData(entity, obj);
-		return reporistory.save(entity);
+		return repository.save(entity);
 	}
 	
 	private void updateData(User entity, User obj) {
