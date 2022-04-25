7571e93244201e75229161d7b107d4fbc98adedf
Wellington Brito <wbrito.taking@alelo.com.br>
2022-04-25 10:19:11 -0300

    adicionando index.html e criando POST para categories

--- src/main/java/com/wellington/curso/resources/CategoryResource.java
+++ src/main/java/com/wellington/curso/resources/CategoryResource.java
@@ -1,13 +1,17 @@
 package com.wellington.curso.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.wellington.curso.entities.Category;
 import com.wellington.curso.services.CategoryService;
@@ -29,6 +33,13 @@ public class CategoryResource {
 	public ResponseEntity<Category> findById(@PathVariable Long id){
 		Category obj = categoryService.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Category> insert(@RequestBody Category category){
+		Category obj = categoryService.insert(category);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 	
 }
--- src/main/java/com/wellington/curso/services/CategoryService.java
+++ src/main/java/com/wellington/curso/services/CategoryService.java
@@ -23,4 +23,8 @@ public class CategoryService {
 		Optional<Category> obj = categoryRepository.findById(id);
 		return obj.get();
 	}
+	
+	public Category insert(Category category) {
+		return categoryRepository.save(category);
+	}
 }
